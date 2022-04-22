72af6a9a5b7df8e1a1d02b1e79a8c81f8a216ded
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-22 08:59:25 +0100

    modifying setCount with a function

--- src/components/HookCounterTwo.js
+++ src/components/HookCounterTwo.js
@@ -4,15 +4,36 @@ function HookCounterTwo() {
   const initialCount = 0;
   const [count, setCount] = useState(initialCount);
 
+  const incrementFive = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount((prevCount) => prevCount + 1);
+    }
+  };
+
   return (
     <div>
       Count - {count}
       <br />
-      <button onClick={() => setCount(initialCount)}>Reset</button>
+      {/* <button onClick={() => setCount(initialCount)}>Reset</button>
       <button onClick={() => setCount(count + 1)}>Increase</button>
       <button onClick={() => (count > 0 ? setCount(count - 1) : initialCount)}>
         Decrease
+      </button> */}
+      ***
+      {/* Using a more appropriate way to change count value */}
+      {/* Into the setCount method, we pass in a function that has access to the previous value, and increase that value by 1  */}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increase
       </button>
+      <button
+        onClick={() =>
+          count > 0 ? setCount((prevCount) => prevCount - 1) : initialCount
+        }
+      >
+        Decrease
+      </button>
+      <button onClick={incrementFive}>Increment Five</button>
     </div>
   );
 }
