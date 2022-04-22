4c042007892c3e00d7099db7f227ea53d99c6e70
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-22 08:01:05 +0100

    steps to implementing counter with class component in ClassCounter.js indicated

--- src/components/ClassCounter.js
+++ src/components/ClassCounter.js
@@ -1,5 +1,7 @@
+// step 1 - create a component
 import React, { Component } from "react";
 
+// step 2 - create a state variable and initialize to 0
 class ClassCounter extends Component {
   constructor(props) {
     super(props);
@@ -9,6 +11,7 @@ class ClassCounter extends Component {
     };
   }
 
+  // step 2 - create a method that is capable of setting this state value
   incrementCount = () => {
     this.setState({
       count: this.state.count + 1,
