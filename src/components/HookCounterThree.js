0101ba0dbfb32adc0ac0ca7029a0ce06539f9157
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-24 09:37:10 +0100

    Using objects as a parameter in the useState hook

--- src/components/HookCounterThree.js
+++ src/components/HookCounterThree.js
@@ -1,1 +1,19 @@
 // Using objects as a parameter in the useSeate hook
+import React, { useState } from "react";
+
+function HookCounterThree() {
+  const [name, setName] = useState({ firstName: "", lastName: "" });
+  // A state variable can be an Integer, string, boolean,object or even an array.
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <h2>Your Firstname is - {name.firstName}</h2>
+        <h2>Your Firstname is - {name.lastName}</h2>
+      </form>
+    </div>
+  );
+}
+
+export default HookCounterThree;
