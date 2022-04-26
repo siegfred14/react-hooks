22b56b661c5394f66fa4cc88771da8b919b28551
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-26 18:27:41 +0100

    Calling the Jsx

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import ClassCounter from "./components/ClassCounter";
 import HookCounter from "./components/HookCounter";
 import HookCounterTwo from "./components/HookCounterTwo";
 import HookCounterThree from "./components/HookCounterThree";
+import HookCounterFour from "./components/HookCounterFour";
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter /> */}
-      <HookCounterThree />
+      <HookCounterFour />
     </div>
   );
 }
