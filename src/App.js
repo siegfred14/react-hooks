66949563ab03651db405e22120be68ebd7f2d961
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-27 10:51:38 +0100

    useEffect replacing lifecycle hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ClassCounterOne from "./components/ClassCounterOne";
+import HookCounterOne from "./components/HookCounterOne";
 
 function App() {
   return (
     <div className="App">
-      <ClassCounterOne />
+      {/* <ClassCounterOne /> */}
+      <HookCounterOne />
     </div>
   );
 }
