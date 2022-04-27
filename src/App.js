20f6859da30c7d88c0f2494bbeb3fd8f35e4488e
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-27 09:54:05 +0100

    useState Hooks notes

--- src/App.js
+++ src/App.js
@@ -17,11 +17,3 @@ function App() {
 }
 
 export default App;
-
-// useState Summary
-// - The useState hook lets you add state to functional components
-// - In classes, the state is always an object. With the useState hook, the state doesn't have to be an object.
-// - The useState hook returns an array with two elements.
-// - The first element is the current valueof the state, and the second element is a state setter function.
-// - If new state value depends on the previous state value, You can pass a function to the setter function.
-// - When dealing with objects or arrays, always make sure to spread your state variables and then call the setter function.
