ca74775c6ec3a7685252a8566560771f0d7dd037
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-29 15:01:09 +0100

    useEffect steps added

--- src/components/HookCounterOne.js
+++ src/components/HookCounterOne.js
@@ -20,3 +20,10 @@ export default HookCounterOne;
 // useEffect Hook
 // The Effect Hook lets you perform side effects in functional components
 // it's a close replacement for componentDidMount, componentDidupdate and componentWillUnmount.
+// useEffect runs after every render
+// useEffect is placed inside the component
+
+// steps
+// import useEffect from React,
+// call it within the component,
+// pass in a function which has to be executed after every render of the component
