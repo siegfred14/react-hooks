4efb8a759ca489ed4cf229b1febc2e709c6fa18c
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-30 10:03:08 +0100

    logging mouse positions using useState and useEffect

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import ClassCounterOne from "./components/ClassCounterOne";
 import HookCounterOne from "./components/HookCounterOne";
 import ClassMouse from "./components/ClassMouse";
+import HookMouse from "./components/HookMouse";
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounterOne /> */}
       {/* <HookCounterOne /> */}
-      <ClassMouse />
+      {/* <ClassMouse /> */}
+      <HookMouse />
     </div>
   );
 }
