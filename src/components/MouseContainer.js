548b32911610a993c701d31dbc1b35646a236ab5
Siegfred Samson <siegfredsamson@gmail.com>
2022-05-02 07:36:23 +0100

    notes on cancelling subscriptions and listeners

--- src/components/MouseContainer.js
+++ src/components/MouseContainer.js
@@ -12,3 +12,7 @@ function MouseContainer() {
 }
 
 export default MouseContainer;
+
+// Even though the mouse event is toggled off, the event listener which belongs to that component is still listening and executing
+// When a component is unmounted, be sure to cancel all subscriptions and listeners
+// With class components, we use the componentWillUnmount. Here, we pass a return function in the function that is passed to useEffect (in HookMouse component). This would be executed when the component will unmount
