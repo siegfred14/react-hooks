c8bb8f2659b3bed529064c878cdba329024eef78
Siegfred Samson <siegfredsamson@gmail.com>
2022-05-02 07:25:00 +0100

    MouseContainer.js set up to toggle display

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ClassCounterOne from "./components/ClassCounterOne";
 import HookCounterOne from "./components/HookCounterOne";
 import ClassMouse from "./components/ClassMouse";
 import HookMouse from "./components/HookMouse";
+import MouseContainer from "./components/MouseContainer";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       {/* <ClassCounterOne /> */}
       {/* <HookCounterOne /> */}
       {/* <ClassMouse /> */}
-      <HookMouse />
+      {/* <HookMouse /> */}
+      <MouseContainer />
     </div>
   );
 }
