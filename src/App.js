425d27c0e47d5ca30412e5515f694594248632fd
Siegfred Samson <siegfredsamson@gmail.com>
2022-05-03 09:28:33 +0100

    initial IntervalHookCounter.js to increase counter dynamically with functions and hooks

--- src/App.js
+++ src/App.js
@@ -5,15 +5,12 @@ import HookCounterOne from "./components/HookCounterOne";
 import ClassMouse from "./components/ClassMouse";
 import HookMouse from "./components/HookMouse";
 import MouseContainer from "./components/MouseContainer";
+import IntervalClassCounter from "./components/IntervalClassCounter";
 
 function App() {
   return (
     <div className="App">
-      {/* <ClassCounterOne /> */}
-      {/* <HookCounterOne /> */}
-      {/* <ClassMouse /> */}
-      {/* <HookMouse /> */}
-      <MouseContainer />
+      <IntervalClassCounter />
     </div>
   );
 }
