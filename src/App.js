a0fe7261dcdc84d2911e86a99813a56b15683a5e
Siegfred Samson <siegfredsamson@gmail.com>
2022-05-08 22:56:10 +0100

    using multiple userContext

--- src/App.js
+++ src/App.js
@@ -11,12 +11,15 @@ import DataFetching from "./components/DataFetching";
 import ComponentC from "./components/ComponentC";
 
 export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
 
 function App() {
   return (
     <div className="App">
       <UserContext.Provider value={"Siegfred"}>
-        <ComponentC />
+        <ChannelContext.Provider value={"Samson"}>
+          <ComponentC />
+        </ChannelContext.Provider>
       </UserContext.Provider>
     </div>
   );
