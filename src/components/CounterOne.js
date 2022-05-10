b007d3373026e7e6e3d2ec871c81839457ff4a05
Siegfred Samson <siegfredsamson@gmail.com>
2022-05-10 08:38:32 +0100

    initial useReducer hook

---
+++ src/components/CounterOne.js
@@ -0,0 +1,8 @@
+// useReducer is a hook that is used for state management in React
+// it is an alternative to useState
+// useState is built using useReducer
+// when to use useReducer or useState????
+
+// useReducer is related to reducer functions
+// useReducer(reducer, initialState)
+// reducer(currentState, action)
