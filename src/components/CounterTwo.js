e5e29cfdb566d1fa6f5cd8534481029d261824f9
Siegfred Samson <siegfredsamson@gmail.com>
2022-05-21 23:22:20 +0100

    initial multiple useReducer hooks

--- src/components/CounterTwo.js
+++ src/components/CounterTwo.js
@@ -77,3 +77,6 @@ export default CounterTwo;
 
 // With second scenario, we'll talk about state as an object
 // suppose we want to add another state property, this is easy when our state is an object.
+
+// Multiple useReducers
+// If you need two counters with the exact same state transitions, we use multiple useReducer hooks
