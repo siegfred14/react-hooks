fd3449d1338f1e4f5091ab72bc3f446dae255425
Siegfred Samson <siegfredsamson@gmail.com>
2022-05-27 23:39:45 +0100

    comparism between useState and useReducer restated

--- src/components/DataFetchingTwo.js
+++ src/components/DataFetchingTwo.js
@@ -53,3 +53,11 @@ function DataFetchingTwo() {
 }
 
 export default DataFetchingTwo;
+
+// useState v useReducer
+// Scenario            -          useState            -        useReducer
+// Type of State           Number, String, Boolean         Object or Array
+// No of state transitions        one or two                    too many
+// Related state transitions        No                            Yes
+// Business Logic             No business logic           Complex Business Logic
+// Local v Global state           local                           global
