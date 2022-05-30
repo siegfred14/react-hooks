729d03d9b78bf86d01ed7b8bb5f325271bbeddb7
Siegfred Samson <siegfredsamson@gmail.com>
2022-05-30 23:59:08 +0100

    notes on react render added

--- src/components/DataFetchingTwo.js
+++ src/components/DataFetchingTwo.js
@@ -61,3 +61,15 @@ export default DataFetchingTwo;
 // Related state transitions        No                            Yes
 // Business Logic             No business logic           Complex Business Logic
 // Local v Global state           local                           global
+
+// React does the render work in 2 phases  
+// the render phase and the commit phase
+
+// Render phase
+// 1. Find all elements flagged for update
+// 2. For each flagged phase. it converts JSX to react elements and stores the result.
+// 3. It performs reconciliation - Diff old new tree of React elements (AKA Virtual DOM).
+// 4. It hands over the changes to the next phase
+
+// Commit Phase
+// 1. The changes are applied to the DOM
